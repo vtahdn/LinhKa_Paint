b098bd49bf17b87c6a205603c8340345b43134ae
Đoàn Việt <vtahdn@gmail.com>
2018-10-13 21:19:05 +0700

    Initial Commit

---
+++ LinhKa_PaintTests/LinhKa_PaintTests.swift
@@ -0,0 +1,36 @@
+//
+//  LinhKa_PaintTests.swift
+//  LinhKa_PaintTests
+//
+//  Created by macbook on 10/13/18.
+//  Copyright © 2018 Viet. All rights reserved.
+//
+
+import XCTest
+@testable import LinhKa_Paint
+
+class LinhKa_PaintTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
